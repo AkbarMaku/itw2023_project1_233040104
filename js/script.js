790e814655986ecbfc03f32158b23051a4e45b49
AkbarMaku <aakbarmaku@gmail.com>
2023-12-19 20:42:26 +0700

    mengerjakan project 1

--- js/script.js
+++ js/script.js
@@ -22,3 +22,12 @@ let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+/*========== hamburger menu ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
